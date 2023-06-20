7323e6e1d673da83abe06717d6b5df7aca8640a9
Scott Morrison <scott.morrison@gmail.com>
2023-06-20 18:01:47 +1000

    less ambitious

--- src/Lean/Meta/LevelDefEq.lean
+++ src/Lean/Meta/LevelDefEq.lean
@@ -81,9 +81,10 @@ mutual
         match (← Meta.decLevel? v) with
         | some v => Bool.toLBool <$> isLevelDefEqAux u v
         | none   => return LBool.undef
-    | Level.max u₁ u₂, Level.max v₁ v₂ =>
-      if ← (isLevelDefEqAux u₁ v₁ <&&> isLevelDefEqAux u₂ v₂) <||> (isLevelDefEqAux u₁ v₂ <&&> isLevelDefEqAux u₂ v₁) then
-        return LBool.true
+    -- Handle `max u v =?= max v ?_` for `v` a parameter.
+    | Level.max u₁ (.param u₂), Level.max (.param v₁) (.mvar v₂) =>
+      if u₂ == v₁ then
+        solve u₁ (.mvar v₂)
       else
         return LBool.undef
     | _, _ => return LBool.undef
