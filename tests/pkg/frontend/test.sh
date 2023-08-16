ad0502543e9ac409dfd87d6c5f05951fe4b9806a
Scott Morrison <scott.morrison@gmail.com>
2023-08-16 12:56:19 +1000

    Include test from #2407 as well

--- tests/pkg/frontend/test.sh
+++ tests/pkg/frontend/test.sh
@@ -11,17 +11,22 @@ lake build
 # from a project (e.g. Mathlib), so that we can inject another tactic
 # implemented in the same project into a goal state from the file.
 
-# This works with Lean 4 `master`
+# This already worked prior to lean4#2423.
 lake exe frontend_with_import2 Frontend.Import1 &&
 
 # Check that if we don't import `Frontend.Import2`, we can successfully run
 #   #eval main ["Frontend.Import1"]
 # in the interpreter
 
-# This works with Lean 4 `master`
+# This already worked prior to lean4#2423.
 lake build Frontend.Main_with_eval &&
 
 # However if `Main` has imported `Frontend.Import2`, then
 #   #eval main ["Frontend.Import1"]
-# fails to compiled `Frontend.Import1` in the interpreter:
-lake build Frontend.Main_with_Import2_and_eval
+# fails to compile `Frontend.Import1` in the interpreter
+# prior to lean4#2423.
+lake build Frontend.Main_with_Import2_and_eval &&
+
+# Compiling multiple files with a shared import with an initializer
+# failed prior to lean4#2423:
+lake exe frontend Frontend.Import1 Frontend.Import2
